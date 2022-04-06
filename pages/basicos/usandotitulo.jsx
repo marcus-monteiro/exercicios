78386062f33640ead49c2a0fad3b3e5e41fac73a
Marcus Monteiro <contato@marcusmonteiro.com>
2022-04-06 17:35:41 -0300

    com operador ternário

--- pages/basicos/usandotitulo.jsx
+++ pages/basicos/usandotitulo.jsx
@@ -13,6 +13,11 @@ export default function usandoTitulo() {
                 secundario="Informe seu login e senha!"
                 pequeno={true}
             />
+            <Titulo
+                principal="Página de login"
+                secundario="Informe seu login e senha!"
+                pequeno //nao precisa passar como true, se tiver já é true, senao é branco
+            />
         </div>
     )
 }
