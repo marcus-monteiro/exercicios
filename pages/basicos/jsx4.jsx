52450d0acf38057d1c17f606323ce045acecf429
Marcus Monteiro <contato@marcusmonteiro.com>
2022-04-06 17:01:17 -0300

    jsx4 e desafio lista

--- pages/basicos/jsx4.jsx
+++ pages/basicos/jsx4.jsx
@@ -1,10 +1,11 @@
 export default function jsx4() {
     const subtitulo = "Estou no javascript"
+    const trechoH3 = <h3>{3 * 3} </h3>
     return (
         <div>
             <h1>Integração jS e JSX</h1>
             <h2>{subtitulo}</h2>
-            <h3>{3 * 3} </h3>
+            {trechoH3}
             <h4>{Math.max(13, 39)}</h4>
             <h5>{entre(30, 1, 10)}</h5>
         </div>
